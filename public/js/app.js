168870b929821116b19d5e6462c0386a8c6dedc7
Anup Lamsal <anuplamsal7410@gmail.com>
2020-12-11 20:23:42 +0545

    Setup app for Heroku

--- public/js/app.js
+++ public/js/app.js
@@ -13,16 +13,14 @@ weatherForm.addEventListener("submit", (e) => {
   firstMessage.textContent = "Loading...";
   secondMessage.textContent = "";
 
-  fetch("http://localhost:3000/weather?address=" + location).then(
-    (response) => {
-      response.json().then((data) => {
-        if (data.error) {
-          firstMessage.textContent = data.error;
-        } else {
-          firstMessage.textContent = data.location;
-          secondMessage.textContent = data.forecast;
-        }
-      });
-    }
-  );
+  fetch("/weather?address=" + location).then((response) => {
+    response.json().then((data) => {
+      if (data.error) {
+        firstMessage.textContent = data.error;
+      } else {
+        firstMessage.textContent = data.location;
+        secondMessage.textContent = data.forecast;
+      }
+    });
+  });
 });
